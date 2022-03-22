d911733b74ac2f15e77f9167bdeb0365bcea3689
dennisdinhhung <dennisdinhhung@mgmail.com>
2022-03-22 09:34:34 +0700

    finished edit, delete function

--- form.js
+++ form.js
@@ -1,6 +1,8 @@
 var form = document.querySelector('.form');
 const table = document.querySelector('.table');
 const tableBody = document.querySelector('tbody');
+const submitBtn = document.querySelector('#submit');
+const updateBtn = document.querySelector('#update');
 
 class User{
     constructor(id, user_name, phone_no, address, radio, email, dob, checkedValue, desc) {
@@ -16,8 +18,15 @@ class User{
     }
 }
 
+const getUserById = (id) => {
+    for (let i = 0; i < listUser.length; i++) {
+        if (listUser[i].id == id) return listUser[i];
+    }
+    return null;
+}
+
 let listUser = []
-let id = -1;
+let id = 0;
 
 if (localStorage.getItem('listUser') != null) {
     listUser = JSON.parse(localStorage.getItem('listUser'));
@@ -44,7 +53,7 @@ form.addEventListener('submit', function(e){
 
     let desc = document.querySelector('.input-desc').value;
 
-    id = listUser.length;
+    id = listUser[listUser.length-1].id + 1;
 
     // make new user
     let user = new User(id, user_name, phone_no, address, radio, email, dob, checkedValue, desc);
@@ -88,8 +97,6 @@ renderRow(listUser);
 
 for (let i = 0; i < listUser.length; i++){
     let user = listUser[i];
-    console.log(i)
-    console.log(listUser[i]);
     let editBtn = document.getElementById(`edit-button-${user.id}`);
     let deleteBtn = document.getElementById(`delete-button-${user.id}`);
 
@@ -111,7 +118,7 @@ for (let i = 0; i < listUser.length; i++){
         document.querySelector('.input-email').value = user.email;
         document.querySelector('.dob').value = user.dob;
         document.querySelector('.input-desc').value = user.desc;
-        // fix this
+        
         let checkboxHTML = document.querySelectorAll('input[name=checkbox]');
         for (let i = 0; i < user.checkedValue.length; i++){
             if (user.checkedValue.includes(checkboxHTML[i].value)){
@@ -121,6 +128,77 @@ for (let i = 0; i < listUser.length; i++){
                 checkboxHTML[i].checked = false;
             }
         }
+
+        //change the class type of the buttons
+            //change submit to disabled
+        submitBtn.classList.add('disabled');
+        submitBtn.classList.remove('avail');
+            //change update to avail
+        updateBtn.classList.add('avail');
+        updateBtn.classList.remove('disabled');
     }
+
+    deleteBtn.onclick = (e) => {
+        e. preventDefault();
+
+        listUser.splice(listUser.indexOf(user), 1);
+
+        renderRow(listUser);
+        location.reload();
+    }
+}
+
+
+//function for update onlick
+updateBtn.onclick = (e) => {
+    e.preventDefault();
+
+    let id = document.querySelector('.id').value;
+    let user_name = document.querySelector('.input-name').value;
+    console.log(user_name);
+    let phone_no = document.querySelector('.input-phone').value;
+    let address = document.querySelector('.address').value;
+    let radio = document.querySelector('input[name="radio"]:checked').value;
+    let email = document.querySelector('.input-email').value;
+    let dob = document.querySelector('.dob').value;
+
+    /* get all checked checkbox */
+    let checkbox = document.querySelectorAll('input[name="checkbox"]');
+    let checkedValue = [];
+    for (let i = 0; i < checkbox.length; i++){
+        if (checkbox[i].checked){
+            checkedValue.push(checkbox[i].value);
+        }
+    }
+
+    let desc = document.querySelector('.input-desc').value;
+
+    // find the user that is being updated
+    let updateUser = getUserById(id);
+
+    // update the info of the updateUser
+    updateUser.user_name = user_name;
+    updateUser.phone_no = phone_no;
+    updateUser.address = address;
+    updateUser.radio = radio;
+    updateUser.email = email;
+    updateUser.dob = dob;
+    updateUser.checkedValue = checkedValue;
+    updateUser.desc = desc;
+    
+    // find the index of the updating user
+    let index = listUser.indexOf(updateUser);
+    // let the OG user = updateUser
+    listUser[index] = updateUser;
+
+    // render the row
+    renderRow(listUser);
+
+    //change the buttons
+    submitBtn.classList.add('btn-active');
+    submitBtn.classList.remove('btn-inactive');
+    updateBtn.classList.add('btn-inactive');
+    updateBtn.classList.remove('btn-active');
+    location.reload();
 }
 // TODO: delete button and func
